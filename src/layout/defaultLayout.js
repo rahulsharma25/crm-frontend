c2cfcbf7fa81df1ca36d4244fd1919945118bd37
rahulsharma25 <rahulsharmagbr@gmail.com>
2023-01-14 18:18:15 +0530

    added dashboard UI layout

---
+++ src/layout/defaultLayout.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Footer } from './partials/footer';
+import { Header } from './partials/header';
+
+export const DefaultLayout = (props) => {
+  return (
+    <div className='default-layout'>
+      <header className='header'>
+        <Header />
+      </header>
+
+      <main className='main'>
+        {props.children}
+      </main>
+
+      <footer className='footer'>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+DefaultLayout.propTypes = {
+  children: PropTypes.object.isRequired
+};
---
+++ src/layout/partials/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <div className='text-center copyright'>
+      &copy; 2023 all rights reserved
+    </div>
+  );
+};
